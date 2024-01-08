4ef21182554d19a110aea249f1faa2ab8c3392cf
Mialy05 <mialisoamurielle@gmail.com>
2024-01-08 10:37:48 +0300

    feat [ROUTER] initialisation router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import TestComponent from "./components/test/test.component.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const routes = createBrowserRouter([
+  {
+    path: "",
+    element: (
+      <App>
+        <Outlet />
+      </App>
+    ),
+    children: [
+      {
+        path: "test",
+        element: <TestComponent />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
