8240b9248821b26cc5f486da0034a2f4df771a1c
ETU1906 <yoanrazafinjaka@gmail.com>
2024-01-11 11:24:00 +0300

    fix [integration] nampina servicepour etat  couleur  categorie  energie

--- src/components/crud/energie/components/energie-form.components.tsx
+++ src/components/crud/energie/components/energie-form.components.tsx
@@ -7,6 +7,7 @@ import "./couleur-form.component.css";
 import "./couleur-form.component.scss";
 import { Url_api } from "../../../shared/constants/global";
 import { Link } from 'react-router-dom';
+import { insertEnergie, updateEnergie } from "../../../service/energie.service";
 
 interface EnergieFormProps {
   entity?: Energie;
@@ -14,8 +15,6 @@ interface EnergieFormProps {
 
 const EnergieFormComponent = (props: EnergieFormProps) => {
   const [state, setState] = useState<EnergieFormState>(initialState);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
   useEffect(() => {
     if (props.entity) {
       setState((state) => ({
@@ -31,45 +30,41 @@ const EnergieFormComponent = (props: EnergieFormProps) => {
     console.log("ny alefa : ");
     console.log( state );
     try {
-      const method_name = energie ? "PUT" : "POST";
-      const response = await fetch(Url_api + "energies", {
-        method: method_name,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(state.form),
-      });
-
-      if (!response.ok) {
-        const responseData = await response.json();
-        const errorMessageFromAPI = responseData.err || "Une erreur s'est produite";
-        setErrorMessage(errorMessageFromAPI);
-        setSuccessMessage(null);
-        return;
+      if (state.form.id) {
+        await updateEnergie(state.form);
+        console.log("Mise à jour effectuée avec succès!");
+        setState((state) => ({ ...state, success: "Modifié avec succès !", error: null }));
+      } else {
+        await insertEnergie(state.form);
+        console.log("Insertion effectuée avec succès!");
+        setState((state) => ({ ...state, success: "Insertion effectuée avec succès !", error: null }));
       }
-
-      setSuccessMessage("Inseré avec succès !");
-      setErrorMessage(null);
-      console.log("Form submitted successfully!" + response);
     } catch (error) {
       console.error("An error occurred:", error);
-      setErrorMessage("Une erreur s'est produite");
-      setSuccessMessage(null);
+      setState((state) => ({ ...state, error: "Une erreur s'est produite", success: null }));
     }
   };
 
   const energie = props.entity;
 
   return (
-    <div className="form-temp energie-form">
+    <div className="form-temp couleur-form">
       <div className="container-form" > 
       <Link to="/energies">
           <i className="form-return fas fa-arrow-left"></i>
         </Link>        <div className="title-form" > 
           <Title >{energie ? "Modifier energie" : "Créer energie"}</Title>
         </div>
-        {errorMessage && <div className="success-error-form" style={{ color: 'red' }}>{errorMessage}</div>}
-        {successMessage && <div className="success-error-form" style={{ color: 'green' }}>{successMessage}</div>}
+        {state.error && (
+          <div className="success-error-form" style={{ color: "red" }}>
+            {state.error}
+          </div>
+        )}
+        {state.success && (
+          <div className="success-error-form" style={{ color: "green" }}>
+            {state.success}
+          </div>
+        )}
         <div className="form">
           <TextField
             label="Nom"
@@ -84,7 +79,6 @@ const EnergieFormComponent = (props: EnergieFormProps) => {
             }
             value={state.form.nom}
           />
-          
           <Button variant="contained" onClick={handleSubmit}>
             {energie ? "Modifier" : "Créer"}
           </Button>
@@ -100,8 +94,10 @@ interface EnergieFormState {
 
 const initialState: EnergieFormState = {
   form: {
-    nom: ""
+    nom: "",
   },
+  success: null,
+  error: null,
 };
 
 export default EnergieFormComponent;
---
+++ src/components/service/etat.service.tsx
@@ -0,0 +1,32 @@
+// apiService.js
+import axios from "axios";
+import { Etat } from "../shared/types/Etat";
+import { Url_api } from "../shared/constants/global";
+
+const updateEtat = async (form : Etat ) => {
+  try {
+    const response = await axios.put(`${Url_api}etats/${form.id}`, form, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const insertEtat = async (form : Etat ) => {
+  try {
+    const response = await axios.post(`${Url_api}etats`, form, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { updateEtat, insertEtat };
