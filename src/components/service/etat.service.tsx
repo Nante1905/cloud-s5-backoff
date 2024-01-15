37da0ca5378d32321604743bf17b9e46f571fb3b
Mialy05 <mialisoamurielle@gmail.com>
2024-01-15 16:09:41 +0300

    fix [CRUD ETAT] enlever ajouter etat

--- src/components/service/etat.service.tsx
+++ src/components/service/etat.service.tsx
@@ -11,6 +11,3 @@ export const findEtatById = (id: number) => axios.get(`${Url_api}/etats/${id}`);
 
 export const updateEtat = async (form: Etat) =>
   axios.put(`${Url_api}/etats/${form.id}`, form);
-
-export const insertEtat = async (form: Etat) =>
-  axios.post(`${Url_api}/etats`, form);
