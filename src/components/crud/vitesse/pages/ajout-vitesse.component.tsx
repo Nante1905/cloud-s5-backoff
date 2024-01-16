9d3cb5c3ae6c0e6759e54cffbbcc54ce1a68e5e1
Nante <nantemino15@gmail.com>
2024-01-16 09:25:07 +0300

    fix: [CRUD VITESSE] fix PR#16

---
+++ src/components/crud/vitesse/pages/ajout-vitesse.component.tsx
@@ -0,0 +1,8 @@
+import VitesseFormComponent from "../components/vitesse-form.component";
+
+const AjoutVitesseComponent = () => {
+  document.title = "Vitesses";
+  return <VitesseFormComponent />;
+};
+
+export default AjoutVitesseComponent;
